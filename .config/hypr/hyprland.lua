006588ff97d6100999aaa38e6c23a2f90d91cd12
wiktrek <wiktor.janiszewski10@gmail.com>
2026-05-24 22:55:48 +0200

    feat: switched waybar position to bottom

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -3,4 +3,7 @@
 require("conf")
 require("keybinds")
 require("monitors")
+hl.on("hyprland.start", function ()
+    hl.exec_cmd("waybar")
+end)
 -- hl.bind("SUPER", "M", "exec", "notify-send LUA_WORKS")
